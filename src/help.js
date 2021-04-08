703730c719e48613aeb9a18058a505d541244ddd
Valky23 <82124403+Valky23@users.noreply.github.com>
2021-04-07 20:06:02 -0500

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,63 @@
+const help = (prefix) => {
+	return `
+「 *Valky Bot* 」
+
+◪ *Información*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Creador: Valky
+  ❏ Instagram: https://www.instagram.com/valkyfamily/
+
+◪ *Para usar al bot*
+  ❏ Registrate con el comando ${prefix}daftar Nombre
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+
+◪ *CREAR*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  └─ ❏ ${prefix}tomp3 
+
+◪ *DESCARGAS*
+  │
+  └─ ❏ ${prefix}ytmp4
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ ❏ ${prefix}bahasa
+  └─ ❏ ${prefix}tts
+
+◪ *DUEÑO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex (Hernqn)
+  ├─ ❏ ${prefix}exe
+  └─ ❏ ${prefix}qrcode
+  
+◪ *NSWF*
+  │
+  └─ ❏ ${prefix}nsfwmenu
+
+     ❏ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw1 y para desactivar los NSFW coloque ${prefix}nsfw0
+
+`
+}
+
+exports.help = help
